db1792f045c89f34e0d793e400de8c8238aa8f6f
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-05 16:14:56 -0500

    coding the header section

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import "./index.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container">
+        <nav>
+          <a href="#">
+            <span>Hernán Mercado</span>
+          </a>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Skills</a>
+            </li>
+            <li>
+              <a href="#">Background</a>
+            </li>
+            <li>
+              <a href="#">Projects</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
