930be5c4ed5c37aa664e9d53b0a86d5062bd6f9d
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-07 20:15:56 -0500

    skills section html

--- src/containers/Skills/index.jsx
+++ src/containers/Skills/index.jsx
@@ -4,7 +4,21 @@ import "./index.css";
 const Skills = () => {
   return (
     <section className="skills">
-      <h2>Skills</h2>
+      <h2 className="skills__title">My skills</h2>
+      <div className="skills__container">
+        <div className="skills__categories">
+          <div className="category">Frontend</div>
+          <div className="category">Backend</div>
+          <div className="category">Design</div>
+        </div>
+      </div>
+      <div className="skills__logo-container">
+        <img src="" alt="technologie logo" />
+        <img src="" alt="technologie logo" />
+        <img src="" alt="technologie logo" />
+        <img src="" alt="technologie logo" />
+      </div>
+      .skills-descri
     </section>
   );
 };
