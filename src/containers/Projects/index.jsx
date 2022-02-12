ff0b5f096ac4de611f2ebd08b30dc6acf434cdfc
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-12 18:12:43 -0500

    styling project card

--- src/containers/Projects/index.jsx
+++ src/containers/Projects/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Title from "../../components/Title";
 import { Icon } from "../../components/Icon/Icon";
+import ProjectImg from "../../assets/projects/budget-manager.png";
 import "./index.css";
 
 const Projects = () => {
@@ -9,6 +10,7 @@ const Projects = () => {
       <Title title={"Projects"} />
       <div className="container projects-container">
         <article className="project-item">
+          <img className="project-image" src={ProjectImg} alt="project image" />
           <div className="project-description">
             <h5 className="project-description__title">FinAntApp</h5>
             <span className="project-description__resume">
@@ -16,6 +18,14 @@ const Projects = () => {
             </span>
             <div className="icon-container">
               <Icon name="react" className="skill__icons-color" />
+            </div>
+            <div className="project-links">
+              <a href="https://friendly-bhabha-bbe9e2.netlify.app/">
+                Preview demo
+              </a>
+              <a href="https://github.com/Hernan1ro/budget-control-app">
+                View code
+              </a>
             </div>
           </div>
         </article>
