ffe64406947c8a6d921999822bcd0e04754c995f
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-11 21:23:30 -0500

    creating project cart and adding project data schema

--- src/containers/Projects/index.jsx
+++ src/containers/Projects/index.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import Title from "../../components/Title";
+import { Icon } from "../../components/Icon/Icon";
+import "./index.css";
 
 const Projects = () => {
   return (
-    <section>
+    <section className="projects-section">
       <Title title={"Projects"} />
+      <div className="container projects-container">
+        <article className="project-item">
+          <div className="project-description">
+            <h5 className="project-description__title">FinAntApp</h5>
+            <span className="project-description__resume">
+              Ant expenses manager to track your personal finance
+            </span>
+            <div className="icon-container">
+              <Icon name="react" className="skill__icons-color" />
+            </div>
+          </div>
+        </article>
+      </div>
     </section>
   );
 };
