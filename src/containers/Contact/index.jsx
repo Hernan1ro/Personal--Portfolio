ff6261c96bfbf305786b801333abcca0838aa3ae
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-15 00:31:17 -0500

    styling contact section

--- src/containers/Contact/index.jsx
+++ src/containers/Contact/index.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 import "./index.css";
 import Title from "../../components/Title";
+import contactImg from "../../assets/contact-pink.jpg";
 
 const Contact = () => {
   return (
-    <section id="contact">
-      <div className="contenedor">
-        <Title title="Contact" />
+    <section id="contact" className="contact-section">
+      <Title title="Contact" />
+      <div className="container">
+        <div className="contact-container">
+          <img
+            className="contact-container__img"
+            src={contactImg}
+            alt="contact-img"
+          />
+          <div className="contact-form">
+            <h3>Let's work together!</h3>
+            <span>Send me a message so we can be in touch</span>
+            <form action=""></form>
+          </div>
+        </div>
       </div>
     </section>
   );
