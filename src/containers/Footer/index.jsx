a827ad295794010508f027dbd76ebeb30d107e3f
Hernán D. Mercado <hernandmf@gmail.com>
2022-02-16 23:52:38 -0500

    contact information html

--- src/containers/Footer/index.jsx
+++ src/containers/Footer/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import PinIcon from "../../components/Icon/PinIcon.jsx";
+import EmailIcon from "../../components/Icon/Email.jsx";
 
 const Footer = () => {
   return (
     <footer>
-      <div className="contact-info"></div>
+      <div className="contact-info">
+        <div className="location">
+          <PinIcon />
+          <span>Fundación - Magdalena</span>
+          <span>Colombia</span>
+        </div>
+        <div className="email">
+          <EmailIcon />
+          <span>Hernandmf@gmail.com</span>
+        </div>
+      </div>
       <div className="resume-info"></div>
       <div className="social-media"></div>
       <div className="copyright"></div>
