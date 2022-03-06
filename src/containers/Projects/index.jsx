b4772dd311db3dd4388748a9a5f2f084cb5e6bc5
Hernán D. Mercado <hernandmf@gmail.com>
2022-03-05 19:59:05 -0500

    making the project category selector

--- src/containers/Projects/index.jsx
+++ src/containers/Projects/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Title from "../../components/Title";
 import { Icon } from "../../components/Icon/Icon";
 import projectImg from "../../assets/projects/budget-manager.png";
@@ -6,15 +6,81 @@ import ProjectCard from "../../components/ProjectCard";
 import "./index.css";
 
 const Projects = () => {
+  const [allActive, setAllActive] = useState(true);
+  const [reactActive, setReactActive] = useState(false);
+  const [javascriptActive, setJavascriptActive] = useState(false);
+  const [nodeActive, setNodeActive] = useState(false);
+  const [filter, setFilter] = useState("all");
+  console.log(filter);
+  const desactivateCategories = () => {
+    setAllActive(false);
+    setReactActive(false);
+    setJavascriptActive(false);
+    setNodeActive(false);
+  };
+  const filterCategory = (category) => {
+    desactivateCategories();
+    setFilter(category);
+  };
+
+  const selectCategory = (category) => {
+    switch (category) {
+      case "all":
+        filterCategory("all");
+        setAllActive(true);
+        break;
+      case "react":
+        filterCategory("react");
+        setReactActive(true);
+        break;
+      case "node":
+        filterCategory("node");
+        setNodeActive(true);
+        break;
+      case "javascript":
+        filterCategory("javascript");
+        setJavascriptActive(true);
+        break;
+    }
+  };
+
   return (
     <section id="projects" className="projects-section">
       <Title title={"Projects"} />
       <div className="container">
         <div className="projects-categories">
-          <div className="project-category project-category-active">All</div>
-          <div className="project-category">React</div>
-          <div className="project-category">Javascript</div>
-          <div className="project-category">NodeJS</div>
+          <div
+            className={`project-category ${
+              allActive ? "project-category-active" : null
+            }`}
+            onClick={() => selectCategory("all")}
+          >
+            All
+          </div>
+          <div
+            className={`project-category ${
+              reactActive ? "project-category-active" : null
+            }`}
+            onClick={() => selectCategory("react")}
+          >
+            React
+          </div>
+          <div
+            className={`project-category ${
+              javascriptActive ? "project-category-active" : null
+            }`}
+            onClick={() => selectCategory("javascript")}
+          >
+            Javascript
+          </div>
+          <div
+            className={`project-category ${
+              nodeActive ? "project-category-active" : null
+            }`}
+            onClick={() => selectCategory("node")}
+          >
+            NodeJS
+          </div>
         </div>
         <div className=" projects-container">
           <ProjectCard Icon={Icon} projectImg={projectImg} />
