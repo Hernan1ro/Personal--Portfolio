a2c173b0240fedb515419f4f1544483b090d409e
Hernán D. Mercado <hernandmf@gmail.com>
2022-03-09 17:15:11 -0500

    fixing the description of the projects

--- src/containers/Projects/ProjectsData.js
+++ src/containers/Projects/ProjectsData.js
@@ -40,7 +40,7 @@ export const ProjectsData = [
     category: ["all", "react"],
     image: usaMachinery,
     description:
-      "Website of USA Machinery company showing their services such as sale of spare parts and technical service to customers with equipment",
+      "Website of USA Machinery Company, buy/rent machinery and get different services.",
     icon: ["html", "css", "react"],
     github: "https://github.com/Hernan1ro/USA-machinery",
     demo: "https://amazing-kirch-7a99e6.netlify.app/",
@@ -51,7 +51,7 @@ export const ProjectsData = [
     category: ["all", "react"],
     image: budgetManager,
     description:
-      "A web app that helps you tu track and analyze your personal budget, built it with React.js and Vite",
+      "A web app that helps you to track and analyze your personal budget.",
     icon: ["html", "css", "react"],
     github: "https://github.com/Hernan1ro/budget-control-app",
     demo: "https://friendly-bhabha-bbe9e2.netlify.app/",
@@ -61,8 +61,7 @@ export const ProjectsData = [
     name: "Travel Agency Web",
     category: ["all", "node"],
     image: travelAgency,
-    description:
-      "A web page that displays some of the most beutiful places of the world and allows customers to book their trips",
+    description: "Book your trip to the most beautiful places in the world.",
     icon: ["html", "css", "node", "mysql", "express"],
     github: "https://github.com/Hernan1ro/TravelAgency",
     demo: "https://cryptic-brushlands-37446.herokuapp.com/",
@@ -72,8 +71,7 @@ export const ProjectsData = [
     name: "FinAntApp",
     category: ["all", "react"],
     image: finanApp,
-    description:
-      "An app that provides solutions related to personal finance and budget management",
+    description: "Track your ant expenses and improve your personal finances.",
     icon: ["react", "css", "redux", "firebase", "chartjs"],
     github: "https://github.com/Hernan1ro/Budget-manager-app",
     demo: "https://angry-fermat-bc349f.netlify.app/",
@@ -84,7 +82,7 @@ export const ProjectsData = [
     category: ["all"],
     image: batatabit,
     description:
-      "This project is the result of practicing HTML and CSS, and it works as a prototype for a digital product",
+      "A HTML & CSS template for a company, built by the mobile first principle. ",
     icon: ["html", "css"],
     github: "https://github.com/Hernan1ro/Batatabit-web-page",
     demo: "https://suspicious-pare-a7d2df.netlify.app/",
@@ -95,8 +93,8 @@ export const ProjectsData = [
     category: ["all"],
     image: brand,
     description:
-      "This project is the result of practicing HTML and CSS, and it works as a prototype for a digital product",
-    icon: ["html", "css", "javascript"],
+      "Website built to practice HTML & CSS with a button linked to paypal.",
+    icon: ["html", "css"],
     github: "https://github.com/Hernan1ro/Brand",
     demo: "https://youthful-goldwasser-956084.netlify.app/",
   },
@@ -106,7 +104,7 @@ export const ProjectsData = [
     category: ["all", "react"],
     image: petgram,
     description:
-      "This project imitates a social media such as Instagram but in this case it was created for pets, Petgram.",
+      "Social media such as Instagram but in this case it was created for pets.",
     icon: ["html", "sass", "javascript", "react", "styledcomponents"],
     github: "https://github.com/Hernan1ro/Petgram",
     demo: "https://peaceful-bartik-228a2a.netlify.app/",
@@ -117,7 +115,7 @@ export const ProjectsData = [
     category: ["all", "javascript"],
     image: carBudgetCalculator,
     description:
-      "An app that helps you to quete your Car insurance depending on their brand, year, and origin",
+      "Quote your Car insurance depending on its brand, year, and origin.",
     icon: ["html", "css", "javascript"],
     github: "https://github.com/Hernan1ro/Car_insurance_quote",
     demo: "https://thirsty-thompson-4133e4.netlify.app/",
@@ -128,7 +126,7 @@ export const ProjectsData = [
     category: ["all", "javascript"],
     image: criptoQuery,
     description:
-      "This is a project in wich you can query the most popular cryptocurrencies in the word and see their prices in different currencies like dollar and Eur",
+      "Query the most popular cryptocurrencies in the word with this app.",
     icon: ["html", "css", "javascript"],
     github: "https://github.com/Hernan1ro/Cryptocurrency-query",
     demo: "https://determined-goldberg-b71032.netlify.app/",
@@ -138,7 +136,7 @@ export const ProjectsData = [
     name: "Loan Calculator",
     category: ["all", "javascript"],
     image: loanCalculator,
-    description: "A web calculator that helps you to calculate your loans",
+    description: "A web calculator that helps you to calculate your loans.",
     icon: ["html", "css", "react"],
     github: "https://github.com/Hernan1ro/loan-app",
     demo: "https://dazzling-kilby-a7bad1.netlify.app/",
@@ -149,7 +147,7 @@ export const ProjectsData = [
     category: ["all", "react"],
     image: gatsbyHotel,
     description:
-      "A demo project in wich y use Gatsby and React to create a website for a hotel",
+      "A demo project that has Gatsby and React to create a website for a hotel.",
     icon: ["html", "css", "react"],
     github: "https://github.com/Hernan1ro/Gatsby-hotel",
     demo: "https://wizardly-gates-c4ed08.netlify.app/",
@@ -160,7 +158,7 @@ export const ProjectsData = [
     category: ["all", "javascript"],
     image: carSeeker,
     description:
-      "Sort cars by brand, year, price and other kind of caracteristics",
+      "Sort cars by brand, year, price and other kind of caracteristics.",
     icon: ["html", "css", "javascript"],
     github: "https://github.com/Hernan1ro/car-seeker/tree/master",
     demo: "https://dazzling-bassi-7fca28.netlify.app/",
@@ -171,7 +169,7 @@ export const ProjectsData = [
     category: ["all", "javascript"],
     image: crmIndexDB,
     description:
-      "A basic CRM made with indexDB to build a CRUD App, made 100% with javascript to challenge my progamming skills",
+      "A basic CRM made with indexDB to build a CRUD App, made 100% with javascript.",
     icon: ["html", "css", "javascript"],
     github: "https://github.com/Hernan1ro/CRM_indexDB",
     demo: "https://hopeful-lamport-29349b.netlify.app/",
@@ -182,7 +180,7 @@ export const ProjectsData = [
     category: ["all"],
     image: materializeWeb,
     description:
-      "In this project i used the Materialize Framework to create a simple layout for fun",
+      "In this project I implemented Materialize to create a simple layout for practice.",
     icon: ["html", "css"],
     github: "https://github.com/Hernan1ro/materialize-project",
     demo: "https://ecstatic-aryabhata-4f95f3.netlify.app/",
@@ -193,7 +191,7 @@ export const ProjectsData = [
     category: ["all", "javascript"],
     image: emailSimulator,
     description:
-      "Its a basic form in which i applied vainilla Js to create a email simulator",
+      "Its a basic form in which i applied vainilla Js to create a email simulator.",
     icon: ["html", "css", "javascript"],
     github: "https://github.com/Hernan1ro/Email-simulator",
     demo: "https://fervent-mccarthy-b82e8c.netlify.app/",
@@ -204,7 +202,7 @@ export const ProjectsData = [
     category: ["all", "react"],
     image: platziVideo,
     description:
-      "This a website to stream diferent kind of videos, it has been made with React. js, Redux, and React-router",
+      "Website that has been made with React. js, Redux, and React-router.",
     icon: ["html", "css", "react", "redux"],
     github: "https://github.com/Hernan1ro/VideoStream",
     demo: "https://kind-thompson-871be5.netlify.app/",
@@ -214,7 +212,7 @@ export const ProjectsData = [
     name: "Youtube clon",
     category: ["all"],
     image: youtubeClon,
-    description: "Web site made to practice HTML & CSS",
+    description: "Youtube dark mode clone built with vainilla HTML & CSS.",
     icon: ["html", "css"],
     github: "https://github.com/Hernan1ro/Youtube-layout",
     demo: "https://inspiring-franklin-bb224d.netlify.app/",
@@ -225,7 +223,7 @@ export const ProjectsData = [
     category: ["all", "javascript"],
     image: imageSeeker,
     description:
-      "A project made 100 % in Javascript to show images acording to user's search, take a look at it and try",
+      "Show images acording to user's search, take a look at it and try.",
     icon: ["html", "css", "javascript"],
     github: "https://github.com/Hernan1ro/Image-seeker",
     demo: "https://pedantic-lichterman-35741d.netlify.app/",
@@ -235,8 +233,7 @@ export const ProjectsData = [
     name: "Simon says game",
     category: ["all", "javascript"],
     image: simonSays,
-    description:
-      "Have fun with this simon dice game, made with JavaScript programming language",
+    description: "Have fun with this simon dice game and challenge your mind.",
     icon: ["html", "css", "javascript"],
     github: "https://github.com/Hernan1ro/simon-dice",
     demo: "https://amazing-poincare-a67c00.netlify.app/",
@@ -247,7 +244,7 @@ export const ProjectsData = [
     category: ["all", "javascript"],
     image: musicSeeker,
     description:
-      "A small project that helps you find your favorite songs, listen to them, sing them and enjoy!",
+      "A small project that helps you to find songs, listen to them and enjoy!",
     icon: ["html", "css", "javascript"],
     github: "https://github.com/Hernan1ro/Song-lyrics-seeker",
     demo: "https://gracious-hermann-a3ac3e.netlify.app/",
@@ -258,7 +255,7 @@ export const ProjectsData = [
     category: ["all", "javascript"],
     image: theWebNews,
     description:
-      "This is part of CSS-HTML-JS challenge to prove my developer skills, a site completely responsive which displays the news and comments from a Fake API",
+      "A site completely responsive which displays the news and comments from a Fake API",
     icon: ["html", "css", "javascript"],
     github: "https://github.com/Hernan1ro/The-web-news",
     demo: "https://vibrant-newton-f41dbd.netlify.app/",
@@ -269,7 +266,7 @@ export const ProjectsData = [
     category: ["all", "javascript"],
     image: vetManager,
     description:
-      "An app that allows you to manage your appointments related to vet cases, made with Vainilla Javascript using Classs and functions even IndexDB to save your data",
+      "An app that allows you to manage vet appointments, made with Vainilla Javascript.",
     icon: ["html", "css", "javascript"],
     github: "https://github.com/Hernan1ro/Vet-meeting-manager",
     demo: "https://romantic-goldberg-2919cc.netlify.app/",
@@ -280,7 +277,7 @@ export const ProjectsData = [
     category: ["all", "javascript"],
     image: weekBudgetManager,
     description:
-      "An App to track your personal week budget, its made to enhance my skills with Vainilla Javascript",
+      "App to track your personal week budget, save your data in localstorage.",
     icon: ["html", "css", "javascript"],
     github: "https://github.com/Hernan1ro/Weekly-budget-tracker",
     demo: "https://agitated-wright-efa6d4.netlify.app/",
@@ -291,7 +288,7 @@ export const ProjectsData = [
     category: ["all", "javascript"],
     image: windowsCalculator,
     description:
-      "This a project in which i apply all what i've learned so far about Js",
+      "This a project in which i apply all what i've learned so far about Js.",
     icon: ["html", "css", "javascript"],
     github: "https://github.com/Hernan1ro/Windows-Calculator",
     demo: "https://suspicious-clarke-3368ba.netlify.app/",
@@ -301,8 +298,7 @@ export const ProjectsData = [
     name: "Platzi Conf",
     category: ["all"],
     image: platziConf,
-    description:
-      "This project is made with the Booststrap Framework of the Platzi Booststrap 4 course",
+    description: "This project is made with the Booststrap 4 Framework.",
     icon: ["html", "css"],
     github: "https://github.com/Hernan1ro/booststrap-proyect",
     demo: "https://hernan1ro.github.io/booststrap-proyect/",
@@ -313,10 +309,10 @@ export const ProjectsData = [
     category: ["all", "javascript"],
     image: weather,
     description:
-      "This is a project made with vainilla Javascript using Open Weather API to provide the user with information in real time about the weather depending of user's preference",
+      "Show to the user information in real time about the weather with a wheater API.",
     icon: ["html", "css", "javascript"],
-    github: "https://github.com/Hernan1ro/Windows-Calculator",
-    demo: "https://suspicious-clarke-3368ba.netlify.app/",
+    github: "https://github.com/Hernan1ro/Weather-API-Index",
+    demo: "https://admiring-wiles-051b4f.netlify.app/",
   },
   {
     id: uuidv4(),
@@ -324,7 +320,7 @@ export const ProjectsData = [
     category: ["all", "node"],
     image: storeApi,
     description:
-      "API built with Node.js : API REST with Express.js that can works as a database for a demo project",
+      "API REST with Express.js that can works as a database for a demo project.",
     icon: ["javascript", "node", "express"],
     github: "https://github.com/Hernan1ro/my-store",
     demo: "https://secret-shelf-81091.herokuapp.com/api/v1/products",
@@ -334,7 +330,8 @@ export const ProjectsData = [
     name: "React shop",
     category: ["all", "react"],
     image: yardsale,
-    description: "This a E-comerce demo built in React.js",
+    description:
+      "This a E-comerce demo built in React.js using an external API to get the data.",
     icon: ["javascript", "html", "sass", "react"],
     github: "https://github.com/Hernan1ro/react-shop",
     demo: "https://shopreact.xyz/",
@@ -345,7 +342,7 @@ export const ProjectsData = [
     category: ["all", "react"],
     image: confmerch,
     description:
-      "Website for merch and more products with a button linked to paypal to simulate the buy process in a Ecomerce",
+      "Website for merch with a demo button linked to paypal to simulate the buying process.",
     icon: ["html", "css", "react"],
     github: "https://github.com/Hernan1ro/conf-merch",
     demo: "https://conf-merch-6dce5.web.app/",
@@ -355,7 +352,8 @@ export const ProjectsData = [
     name: "Task Manager",
     category: ["all", "javascript"],
     image: taskManager,
-    description: "Organize your to-do list with this simple app.",
+    description:
+      "Organize your to-do list with this simple app made with vainilla JS.",
     icon: ["html", "css", "javascript"],
     github: "https://github.com/Hernan1ro/Lista-de-tareas",
     demo: "https://hernan1ro.github.io/Lista-de-tareas/",
@@ -366,7 +364,7 @@ export const ProjectsData = [
     category: ["all", "javascript"],
     image: blackjack,
     description:
-      "The classic BlackJack game built with vainilla Javascript to test your luck with the cards",
+      "The BlackJack game built with vainilla Javascript to test your luck with the cards.",
     icon: ["html", "css", "javascript"],
     github: "https://github.com/Hernan1ro/blackjack-game",
     demo: "https://modest-poincare-34531d.netlify.app/",
