a430833696d082ebdbe5f4ac6cb0035ee168b080
Hernán D. Mercado <hernandmf@gmail.com>
2022-03-14 23:05:13 -0500

    fixing scroll effect in project section

--- src/containers/Projects/index.jsx
+++ src/containers/Projects/index.jsx
@@ -14,7 +14,7 @@ const Projects = ({ filter, setFilter, setProjectsVisible }) => {
   useEffect(() => {
     let options = {
       rootMargin: "50px",
-      threshold: 0.05,
+      threshold: 0.03,
     };
     const observer = new IntersectionObserver(function (entries) {
       const { isIntersecting } = entries[0];
