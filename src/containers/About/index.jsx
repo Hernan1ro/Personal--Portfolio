85abdcf35c45e67e30ccb1718de871126b5effcc
Hernán D. Mercado <hernandmf@gmail.com>
2022-03-21 14:06:53 -0500

    modifying the file location to fix deploy

--- src/containers/About/index.jsx
+++ src/containers/About/index.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import "./index.css";
 import profile from "../../assets/profile.jpg";
 import Title from "../../components/Title";
-import SocialMediaLinks from "../../components/SocialMediaLinks";
+import SocialMediaLinks from "../../components/SocialMediaLinks/index.jsx";
 
 const About = ({ setAboutVisible }) => {
   const section = useRef(null);
